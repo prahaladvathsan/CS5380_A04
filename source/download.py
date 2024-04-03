7135bffd4b0a06d235d6a65f210bbe599221e345
Prahalad vathsan <prahalad@smail.iitm.ac.in>
2024-04-03 22:58:39 +0530

    polished download

--- source/download.py
+++ source/download.py
@@ -1,33 +1,43 @@
-import requests
+import requests  
 from bs4 import BeautifulSoup
-import os
-import random
-import pandas as pd
+import os  
+import random  
+import pandas as pd  
 import numpy as np
-import yaml
+import yaml  
 
+# Suppress warnings for cleaner output
 import warnings
 warnings.filterwarnings('ignore')
 
+# Define the main function
+def fetch_weather_data():
+    # Load parameters from the YAML configuration file
+    config_params = yaml.safe_load(open("params.yaml"))["download"]
 
+    # Construct the URL from parameters
+    year_path = str(config_params['year']) + '/'
+    location_file = str(config_params['n_loc']) + '.csv'
+    base_url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/'
 
-def main():
-    params = yaml.safe_load(open("params.yaml"))["download"]
-
-    year = str(params['year'])+'/'
-    n_loc = str(params['n_loc'])+'.csv'
-
-    url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/'
+    # Combine parts to form the complete URL to download the data
+    complete_url = os.path.join(base_url, year_path, location_file)
     
+    # Perform the HTTP GET request to fetch the data
+    data_response = requests.get(complete_url)
 
-    main_url = os.path.join(url,year,n_loc)
-    response = requests.get(main_url)
+    # Define the name and path for the local file to save the data
+    local_data_filename = 'weatherdata.csv'
+    data_directory = 'data'
 
-    local_filename = 'weather.csv'
-
-    os.makedirs('data',exist_ok=True)
-    open(os.path.join('data',local_filename),'wb').write(response.content)
+    # Ensure the directory exists
+    os.makedirs(data_directory, exist_ok=True)
     
+    # Write the content to a local file
+    with open(os.path.join(data_directory, local_data_filename), 'wb') as file:
+        file.write(data_response.content)
+
+# Check if the script is executed directly (i.e., not imported)
 if __name__ == "__main__":
-    main()
-
+    # Call the main function to execute the script
+    fetch_weather_data()
